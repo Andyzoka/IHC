fa0cb795f03c77b86e928a8554416f93c52b6a9a
Thauer Furtado <thauer.furtado@gmail.com>
2022-06-08 20:39:14 -0300

    Add react icon on title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>IHC Transporte</title>
-        <link rel="icon" href="../public/react.ico" />
+        <link rel="icon" href="/react.ico" />
       </Head>
 
       <main className={styles.main}>
